4741efad373c6e7d6db476f9ec49ece502f6a8cf
LegionDev370 <llegion630@gmail.com>
2023-08-25 00:27:13 +0500

    commit

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-const express = require("express");
-const app = express();
-
-const number = 20
-
-const saySimba = () => {
-    
-}
-
-const age = 25;
-const word = "Hello, world!";
-
-const sayHello = () => {};
-const func = () => {};
-
-const sayName = () => {
-  console.log("Hello, world!");
-};
+const http = require("http");
